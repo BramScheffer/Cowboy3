40ea01e5c26e757b4f0f680a8c94a4f5c8577b89
BramScheffer <140196370+BramScheffer@users.noreply.github.com>
2024-02-21 12:10:25 +0100

    Reloading

--- Cowboy3/Assets/Scripts/WaponScripts/Gun.cs
+++ Cowboy3/Assets/Scripts/WaponScripts/Gun.cs
@@ -4,42 +4,88 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public UnityEvent OnGunShoot;
+    public UnityEvent OnReload;
+
     public float fireCooldown;
+    public float reloadTime;
+    public int maxAmmo;
+    private int currentAmmo;
     public bool automatic;
     public float currentCooldown;
+    private bool isReloading;
 
     // Start is called before the first frame update
     void Start()
     {
+        currentAmmo = maxAmmo;
         currentCooldown = fireCooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isReloading)
+        {
+            // Handle reloading
+            return;
+        }
+
         if (automatic)
         {
             if (Input.GetMouseButton(0))
             {
-                if (currentCooldown <= 0f)
-                {
-                    OnGunShoot?.Invoke();
-                    currentCooldown = fireCooldown;
-                } 
+                Shoot();
             }
         }
         else
         {
             if (Input.GetMouseButtonDown(0))
             {
-                if (currentCooldown <= 0f)
-                {
-                    OnGunShoot?.Invoke();
-                    currentCooldown = fireCooldown;
-                }
+                Shoot();
             }
         }
+
+        // Handle cooldown
         currentCooldown -= Time.deltaTime;
 
+        // Handle reload input
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+        }
+    }
+
+    void Shoot()
+    {
+        if (currentCooldown <= 0f && currentAmmo > 0)
+        {
+            OnGunShoot?.Invoke();
+            currentCooldown = fireCooldown;
+            currentAmmo--;
+            if (currentAmmo == 0)
+            {
+                Reload();
+            }
+        }
+    }
+
+    void Reload()
+    {
+        if (currentAmmo < maxAmmo)
+        {
+            isReloading = true;
+            OnReload?.Invoke();
+            // You can play reload animation or sound here if needed
+
+            // Reset ammo after reloadTime
+            Invoke("FinishReload", reloadTime);
+        }
+    }
+
+    void FinishReload()
+    {
+        currentAmmo = maxAmmo;
+        isReloading = false;
     }
 }
+
