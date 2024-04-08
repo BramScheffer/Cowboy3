5e42f1d3a11a0e2d5ac6746477eb2b1f53d38ef1
BramScheffer <140196370+BramScheffer@users.noreply.github.com>
2024-04-08 14:02:13 +0200

    Better Materials Props

---
+++ Cowboy3/Assets/Scripts/zombie ai/SpawnerScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject zombiePrefab;
+    public float spawnRadius;
+    public int zombieCount;
+    public int maxZombieCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zombieCount = maxZombieCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (zombieCount < maxZombieCount)
+        {
+            Instantiate(zombiePrefab);
+            Debug.Log("Spawn een nieuwe zombie");
+        }
+    }
+}
