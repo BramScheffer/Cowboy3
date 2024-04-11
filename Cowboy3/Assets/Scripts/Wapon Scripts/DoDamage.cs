c05fad2ea078440479aa121531ff7f19ee18a304
BramScheffer <140196370+BramScheffer@users.noreply.github.com>
2024-04-11 19:10:11 +0200

    Notities in Scripts

--- Cowboy3/Assets/Scripts/Wapon Scripts/DoDamage.cs
+++ Cowboy3/Assets/Scripts/Wapon Scripts/DoDamage.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DoDamage : MonoBehaviour
 {
-    public float Damage;
-    public float BulletRange;
+    public float damage;
+    public float bulletRange;
     private Transform PlayerCamera;
 
     
@@ -18,14 +19,16 @@ public class DoDamage : MonoBehaviour
 
     public void Shoot()
     {
-        
+        //een new Ray betekend dat we een starting punt kunnen vastleggen 
         Ray gunRay = new Ray(PlayerCamera.position, PlayerCamera.forward);
-        if (Physics.Raycast(gunRay, out RaycastHit hitInfo, BulletRange))
+        //als de gunray iets raakt binnen de bullet range
+        //out houd informatie over wat de ray heeft gehit en de hit info stored the information
+        if (Physics.Raycast(gunRay, out RaycastHit hitInfo, bulletRange))
         {
             //checkt of het object dat geraakt is een bepaald component heeft dat entity heet en damage kan opnemen.
             if (hitInfo.collider.gameObject.TryGetComponent (out Entity enemy)) 
             {
-                enemy.Health -= Damage;
+                enemy.Health -= damage;
             }
         }
     }
--- Cowboy3/Assets/Scripts/zombie ai/Entity.cs
+++ Cowboy3/Assets/Scripts/zombie ai/Entity.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
+    //serialize field is dat je het niet kan aanpassen tijdens de game
     [SerializeField] private float StartingHealth;
     private ScoreManager scoreManager;
     public int scoreValue;
+    //health word gebruikt in dit script
     private float health;
+    //Health word gebruikt om in andere scripts te referencen voor andere scripts
     public float Health
     {
+        // get krijgt de info
         get
         {
+            //geeft aan welke waarde moet worden teruggegeven wanneer de eigenschap wordt aangesproken aan het script
             return health;
         }
+        //set veranderd de info
         set
         {
+            //moet het gebruiken om de nieuwe waarde toe te wijzen aan de eigenschap binnen de set accessor
             health = value;
             Debug.Log(health);
 
